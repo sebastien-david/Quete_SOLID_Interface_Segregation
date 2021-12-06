a581707f3136d0e69942c731583a0d8c8142e1f4
Sebastien David <sebastien.david08@gmail.com>
2021-12-06 10:56:00 +0100

    Ajoutez des fichiers projet.

---
+++ Quete_SOLID_Interface_Segregation/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quete_SOLID_Interface_Segregation
+{
+    class Program
+    {
+        interface IDrivable
+        {
+            void Drive(long miles);
+        }
+
+        interface IDrivableLaunchRocket : IDrivable
+        {
+            void LaunchRocket();
+        }
+
+        class Car : IDrivable
+        {
+            public void Drive(long miles)
+            {
+                Console.WriteLine("I am driven to {0} miles away ...", miles);
+            }
+        }
+
+        class FlyingCar : IDrivableLaunchRocket
+        {
+            private bool _isRocketLaunched;
+
+            public void Drive(long miles)
+            {
+                if (!_isRocketLaunched)
+                {
+                    Console.WriteLine("Launch the rocket first or this flying car can not get driven.");
+                }
+                else
+                {
+                    Console.WriteLine("I am flying to {0} miles away ...", miles);
+                }
+            }
+
+            public void LaunchRocket()
+            {
+                _isRocketLaunched = true;
+            }
+        }
+
+        class SpaceShip : IDrivableLaunchRocket
+        {
+            private bool _isRocketLaunched;
+
+            public void Drive(long miles)
+            {
+                if (!_isRocketLaunched)
+                {
+                    Console.WriteLine("Spaceship can't leave Earth if its rocket is not launched !");
+                }
+                else
+                {
+                    Console.WriteLine("The spaceship is going {0} miles away", miles);
+                }
+            }
+
+            public void LaunchRocket()
+            {
+                _isRocketLaunched = true;
+            }
+        }
+    }
+}
